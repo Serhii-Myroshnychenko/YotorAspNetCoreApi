e3e5229665eeacafae89bd4bcbe238dd2f80e914
Serhii-Myroshnychenko <serhii.myroshnychenko@nure.ua>
2021-11-02 00:11:40 +0200

    created repositories

---
+++ YotorAspNetCoreApiResources/Contracts/IRestrictionRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YotorAspNetCoreApiResources.Contracts
+{
+    public class IRestrictionRepository
+    {
+    }
+}
---
+++ YotorAspNetCoreApiResources/Repositories/FeedbackRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YotorAspNetCoreApiResources.Repositories
+{
+    public class FeedbackRepository
+    {
+    }
+}
