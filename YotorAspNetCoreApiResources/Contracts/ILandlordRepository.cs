420fc3476533287182b025bd31e15cee38625c69
Serhii-Myroshnychenko <serhii.myroshnychenko@nure.ua>
2021-11-10 14:52:03 +0200

    create landlord logic

--- YotorAspNetCoreApiResources/Contracts/ILandlordRepository.cs
+++ YotorAspNetCoreApiResources/Contracts/ILandlordRepository.cs
@@ -2,10 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using YotorAspNetCoreApiResources.Models;
 
 namespace YotorAspNetCoreApiResources.Contracts
 {
     public interface ILandlordRepository
     {
+        public Task<IEnumerable<Landlord>> GetLandlords();
+        public Task<Landlord> GetLandlord(int id);
+        public Task UpdateLandlord(int id, Landlord landlord);
+        public Task CreateLandlord(Landlord landlord);
+
+
     }
 }
---
+++ YotorAspNetCoreApiResources/Controllers/LandlordController.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using YotorAspNetCoreApiResources.Contracts;
+using YotorAspNetCoreApiResources.Models;
+
+namespace YotorAspNetCoreApiResources.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LandlordController : ControllerBase
+    {
+        private readonly ILandlordRepository _landlordRepository;
+        private readonly IHelpRepository _helpRepository;
+
+
+        private int UserId => int.Parse(User.Claims.Single(c => c.Type == "user_id").Value);
+        public LandlordController(ILandlordRepository landlordRepository, IHelpRepository helpRepository)
+        {
+            _landlordRepository = landlordRepository;
+            _helpRepository = helpRepository;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetLandlords()
+        {
+            try
+            {
+                bool isAdmin = await _helpRepository.IsAdmin(UserId);
+                if(isAdmin == true)
+                {
+                    var landlords = await _landlordRepository.GetLandlords();
+                    return Ok(landlords);
+                }
+                else
+                {
+                    return BadRequest("Вы не являетесь администратором");
+                }
+            }
+            catch(Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetLandlord(int id)
+        {
+            try
+            {
+                bool isAdmin = await _helpRepository.IsAdmin(UserId);
+                if(isAdmin == true)
+                {
+                    var landlord = await _landlordRepository.GetLandlord(id);
+                    return Ok(landlord);
+                }
+                else
+                {
+                    return BadRequest("Вы не являетесь администратором");
+                }
+            }
+            catch(Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateLandlord(Landlord landlord)
+        {
+            try
+            {
+                bool isAdmin = await _helpRepository.IsAdmin(UserId);
+                if (isAdmin == true)
+                {
+                    await _landlordRepository.CreateLandlord(landlord);
+                    return Ok("Ok");
+                }
+                else
+                {
+                    return BadRequest("Вы не являетесь администратором");
+                }
+            }
+            catch(Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateLandlord()
+        {
+            try
+            {
+                return Ok();
+
+            }
+            catch(Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+    }
+}
